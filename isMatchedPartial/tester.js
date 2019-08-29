d148025b0c04ad088e20aaca1f6a8ebcf0f71122
datalater <datalater@gmail.com>
2019-08-29 18:09:03 +0900

    Delete needless test message

--- isMatchedPartial/tester.js
+++ isMatchedPartial/tester.js
@@ -1,26 +1,23 @@
 var testMessage = {};
-testMessage.start = 'Test Starting...';
+testMessage.success = 'TEST SUCCESS';
+testMessage.fail = 'TEST FAILED';
 testMessage.name = 'Name: \t\t';
-testMessage.success = 'Result: \t\tsuccess';
-testMessage.fail = 'Result: \t\tfail';
 testMessage.expected = 'Expected: \t';
 testMessage.actual = 'Actual: \t\t';
 testMessage.end = "==========EOT==========\n";
 
 function assertSame(testName, expected, actual) {
   if (isSame(expected, actual)) {
-    Logger.log(testMessage.start);
-    Logger.log(testMessage.name + testName);
     Logger.log(testMessage.success);
+    Logger.log(testMessage.name + testName);
     Logger.log(testMessage.expected + expected);
     Logger.log(testMessage.actual + actual);
     Logger.log(testMessage.end);
     return;
   }
   
-  Logger.log(testMessage.start);
-  Logger.log(testMessage.name + testName);
   Logger.log(testMessage.fail);
+  Logger.log(testMessage.name + testName);
   Logger.log(testMessage.expected + expected);
   Logger.log(testMessage.actual + actual);
   Logger.log(testMessage.end);
